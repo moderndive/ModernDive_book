6309adce486255744db0caaae90ff278e4f7d453
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-21 05:46:58 +0000

    Deploy ismayc/moderndiver-book to github.com/ismayc/moderndiver-book.git:gh-pages

---
+++ scripts/05-wrangling.R
@@ -0,0 +1,328 @@
+## ---- message=FALSE------------------------------------------------------
+library(dplyr)
+library(ggplot2)
+library(nycflights13)
+library(knitr)
+
+## ----message=FALSE, warning=FALSE, echo=FALSE----------------------------
+# Packages needed internally, but not in text.
+
+## ---- eval=FALSE---------------------------------------------------------
+## portland_flights <- flights %>%
+##   filter(dest == "PDX")
+## View(portland_flights)
+
+## ---- eval=FALSE---------------------------------------------------------
+## btv_sea_flights_fall <- flights %>%
+##   filter(origin == "JFK", (dest == "BTV" | dest == "SEA"), month >= 10)
+## View(btv_sea_flights_fall)
+
+## ---- eval=FALSE---------------------------------------------------------
+## not_BTV_SEA <- flights %>%
+##   filter(!(dest == "BTV" | dest == "SEA"))
+## View(not_BTV_SEA)
+
+## **Learning Check Solutions**
+
+## ---- eval=FALSE, echo=show_solutions('5-1')-----------------------------
+## # Original in book
+## not_BTV_SEA <- flights %>%
+##   filter(!(dest == "BTV" | dest == "SEA"))
+## 
+## # Alternative way
+## not_BTV_SEA <- flights %>%
+##   filter(!dest == "BTV" & !dest == "SEA")
+## 
+## # Yet another way
+## not_BTV_SEA <- flights %>%
+##   filter(dest != "BTV" & dest != "SEA")
+
+## ------------------------------------------------------------------------
+summary_temp <- weather %>% 
+  summarize(mean = mean(temp), std_dev = sd(temp))
+kable(summary_temp)
+
+## ------------------------------------------------------------------------
+summary_temp <- weather %>% 
+  summarize(mean = mean(temp, na.rm = TRUE), std_dev = sd(temp, na.rm = TRUE))
+kable(summary_temp)
+
+## ------------------------------------------------------------------------
+#summary_temp$mean
+
+## ----eval=FALSE----------------------------------------------------------
+## summary_temp <- weather %>%
+##   summarize(mean = mean(temp, na.rm = TRUE)) %>%
+##   summarize(std_dev = sd(temp, na.rm = TRUE))
+
+## **Learning Check Solutions**
+
+## ---- eval=show_solutions('5-2'), echo=show_solutions('5-2')-------------
+weather %>% 
+  summarize(count = n())
+
+## **`r paste0("(LC", chap, ".", (lc), ")")`** Why doesn't the following code work?
+
+## ----eval=FALSE, include=show_solutions('5-2')---------------------------
+## summary_temp <- weather %>%
+##   summarize(mean = mean(temp, na.rm = TRUE)) %>%
+##   summarize(std_dev = sd(temp, na.rm = TRUE))
+
+## Consider the output of only running the first two lines:
+
+## ---- eval=show_solutions('5-2'), echo=show_solutions('5-2')-------------
+weather %>%   
+  summarize(mean = mean(temp, na.rm = TRUE))
+
+## Because after the first `summarize()`, the variable `temp` disappears as it has been collapsed to the value `mean`. So when we try to run the second `summarize()`, it can't find the variable temp` to compute the standard deviation of.
+
+## ------------------------------------------------------------------------
+summary_monthly_temp <- weather %>% 
+  group_by(month) %>% 
+  summarize(mean = mean(temp, na.rm = TRUE), 
+            std_dev = sd(temp, na.rm = TRUE))
+kable(summary_monthly_temp)
+
+## ------------------------------------------------------------------------
+by_origin <- flights %>% 
+  group_by(origin) %>% 
+  summarize(count = n())
+kable(by_origin)
+
+## ------------------------------------------------------------------------
+by_monthly_origin <- flights %>% 
+  group_by(origin, month) %>% 
+  summarize(count = n())
+kable(by_monthly_origin)
+
+## ------------------------------------------------------------------------
+by_monthly_origin2 <- flights %>% 
+  dplyr::count(origin, month)
+kable(by_monthly_origin2)
+
+## **_Learning check_**
+
+## **Learning Check Solutions**
+
+## ---- echo=show_solutions('5-2')-----------------------------------------
+library(dplyr)
+library(nycflights13)
+
+summary_temp_by_month <- weather %>% 
+  group_by(month) %>% 
+  summarize(
+          mean = mean(temp, na.rm = TRUE),
+          std_dev = sd(temp, na.rm = TRUE)
+          )
+
+## ---- echo=FALSE, include=show_solutions('5-3')--------------------------
+kable(summary_temp_by_month)
+
+## The standard deviation is a quantification of **spread** and **variability**. We
+
+## ---- echo=show_solutions('5-3'), include=show_solutions('5-3')----------
+summary_temp_by_day <- weather %>% 
+  group_by(year, month, day) %>% 
+  summarize(
+          mean = mean(temp, na.rm = TRUE),
+          std_dev = sd(temp, na.rm = TRUE)
+          )
+summary_temp_by_day
+
+## Note: `group_by(day)` is not enough, because `day` is a value between 1-31. We need to `group_by(year, month, day)`
+
+## ---- echo=show_solutions('5-3'), include=show_solutions('5-3')----------
+by_monthly_origin <- flights %>% 
+  group_by(month, origin) %>% 
+  summarize(count = n())
+
+## ---- eval=FALSE, echo=show_solutions('5-3')-----------------------------
+## by_monthly_origin
+
+## ---- echo=FALSE, include=show_solutions('5-3')--------------------------
+kable(by_monthly_origin)
+
+## The difference is they are organized/sorted by `month` first, then `origin`
+
+## ---- echo=show_solutions('5-3'), include=show_solutions('5-3')----------
+count_flights_by_airport <- flights %>% 
+  group_by(origin, month) %>% 
+  summarize(count=n())
+
+## ---- eval=FALSE, include=show_solutions('5-3')--------------------------
+## count_flights_by_airport
+
+## ---- echo=FALSE, include=show_solutions('5-3')--------------------------
+kable(count_flights_by_airport)
+
+## All remarkably similar!
+
+## NA
+## ------------------------------------------------------------------------
+flights <- flights %>% 
+  mutate(gain = dep_delay - arr_delay)
+
+## ------------------------------------------------------------------------
+gain_summary <- flights %>% 
+  summarize(
+    min = min(gain, na.rm = TRUE),
+    q1 = quantile(gain, 0.25, na.rm = TRUE),
+    median = quantile(gain, 0.5, na.rm = TRUE),
+    q3 = quantile(gain, 0.75, na.rm = TRUE),
+    max = max(gain, na.rm = TRUE),
+    mean = mean(gain, na.rm = TRUE),
+    sd = sd(gain, na.rm = TRUE),
+    missing = sum(is.na(gain))
+  )
+kable(gain_summary)
+
+## ----message=FALSE, fig.cap="Histogram of gain variable"-----------------
+ggplot(data = flights, mapping = aes(x = gain)) +
+  geom_histogram(color = "white", bins = 20)
+
+## ------------------------------------------------------------------------
+flights <- flights %>% 
+  mutate(
+    gain = dep_delay - arr_delay,
+    hours = air_time / 60,
+    gain_per_hour = gain / hours
+  )
+
+## **Learning Check Solutions**
+
+## ---- eval---------------------------------------------------------------
+freq_dest <- flights %>% 
+  group_by(dest) %>% 
+  summarize(num_flights = n())
+freq_dest
+
+## ------------------------------------------------------------------------
+freq_dest %>% arrange(num_flights)
+
+## ------------------------------------------------------------------------
+freq_dest %>% arrange(desc(num_flights))
+
+## ----eval=FALSE----------------------------------------------------------
+## View(airlines)
+
+## ----eval=FALSE----------------------------------------------------------
+## flights_joined <- flights %>%
+##   inner_join(airlines, by = "carrier")
+## View(flights)
+## View(flights_joined)
+
+## ----eval=FALSE----------------------------------------------------------
+## View(airports)
+
+## ---- eval=FALSE---------------------------------------------------------
+## flights %>%
+##   inner_join(airports, by = c("dest" = "faa"))
+
+## ---- eval=FALSE---------------------------------------------------------
+## named_dests <- flights %>%
+##   group_by(dest) %>%
+##   summarize(num_flights = n()) %>%
+##   arrange(desc(num_flights)) %>%
+##   inner_join(airports, by = c("dest" = "faa")) %>%
+##   rename(airport_name = name)
+## View(named_dests)
+
+## **Learning Check Solutions**
+
+## ---- eval=FALSE---------------------------------------------------------
+## glimpse(flights)
+
+## ---- eval=FALSE---------------------------------------------------------
+## flights %>%
+##   select(carrier, flight)
+
+## ---- eval=FALSE---------------------------------------------------------
+## flights_no_year <- flights %>%
+##   select(-year)
+## names(flights_no_year)
+
+## ---- eval=FALSE---------------------------------------------------------
+## flight_arr_times <- flights %>%
+##   select(month:day, arr_time:sched_arr_time)
+## flight_arr_times
+
+## ---- eval=FALSE---------------------------------------------------------
+## flights_reorder <- flights %>%
+##   select(month:day, hour:time_hour, everything())
+## names(flights_reorder)
+
+## ---- eval=FALSE---------------------------------------------------------
+## flights_begin_a <- flights %>%
+##   select(starts_with("a"))
+## flights_begin_a
+
+## ---- eval=FALSE---------------------------------------------------------
+## flights_delays <- flights %>%
+##   select(ends_with("delay"))
+## flights_delays
+
+## ---- eval=FALSE---------------------------------------------------------
+## flights_time <- flights %>%
+##   select(contains("time"))
+## flights_time
+
+## ---- eval=FALSE---------------------------------------------------------
+## flights_time_new <- flights %>%
+##   select(contains("time")) %>%
+##   rename(departure_time = dep_time,
+##          arrival_time = arr_time)
+## names(flights_time)
+
+## ---- eval=FALSE---------------------------------------------------------
+## named_dests %>%
+##   top_n(n = 10, wt = num_flights)
+
+## ---- eval=FALSE---------------------------------------------------------
+## named_dests  %>%
+##   top_n(n = 10, wt = num_flights) %>%
+##   arrange(desc(num_flights))
+
+## ---- eval=FALSE---------------------------------------------------------
+## ten_freq_dests <- flights %>%
+##   group_by(dest) %>%
+##   summarize(num_flights = n()) %>%
+##   arrange(desc(num_flights)) %>%
+##   top_n(n = 10)
+## View(ten_freq_dests)
+
+## **Learning Check Solutions**
+
+## ---- echo=show_solutions('5-6'), include=show_solutions('5-6')----------
+library(dplyr)
+library(nycflights13)
+
+## ---- echo=show_solutions('5-6'), include=show_solutions('5-6')----------
+# The regular way:
+flights %>% 
+  select(dest, air_time, distance)
+
+# Since they are sequential columns in the dataset
+flights %>% 
+  select(dest:distance)
+
+# Not as effective, by removing everything else
+flights %>% 
+  select(-year, -month, -day, -dep_time, -sched_dep_time, -dep_delay, -arr_time,
+         -sched_arr_time, -arr_delay, -carrier, -flight, -tailnum, -origin, 
+         -hour, -minute, -time_hour)
+
+## **`r paste0("(LC", chap, ".", (lc - 1), ")")`** Why might we want to use the `select` function on a data frame?
+
+## ---- echo=show_solutions('5-6'), include=show_solutions('5-6')----------
+# Anything that starts with "d"
+flights %>% 
+  select(starts_with("d"))
+# Anything related to delays:
+flights %>% 
+  select(ends_with("delay"))
+# Anything related to departures:
+flights %>% 
+  select(contains("dep"))
+
+## **`r paste0("(LC", chap, ".", (lc-1), ")")`** Create a new data frame that shows the top 5 airports with the largest arrival delays from NYC in 2013. To narrow down the data frame, to make it easier to look at. Using `View()` for example.
+
---
+++ scripts/09-confidence-intervals.R
@@ -0,0 +1,146 @@
+## ----message=FALSE, warning=FALSE----------------------------------------
+library(dplyr)
+library(ggplot2)
+library(mosaic)
+library(knitr)
+library(ggplot2movies)
+
+## ----message=FALSE, warning=FALSE, echo=FALSE----------------------------
+# Packages needed internally, but not in text.
+
+## ----fig.cap="Population ratings histogram"------------------------------
+movies %>% ggplot(aes(x = rating)) +
+  geom_histogram(color = "white", bins = 20)
+
+## **_Learning check_**
+
+## ------------------------------------------------------------------------
+set.seed(2017)
+movies_sample <- movies %>% 
+  sample_n(50)
+
+## ----fig.cap="Sample ratings histogram"----------------------------------
+ggplot(data = movies_sample, aes(x = rating)) +
+  geom_histogram(color = "white", bins = 20)
+
+## ------------------------------------------------------------------------
+(movies_sample_mean <- movies_sample %>% 
+   summarize(mean = mean(rating)))
+
+## ------------------------------------------------------------------------
+boot1 <- resample(movies_sample) %>%
+  arrange(orig.id)
+
+## ------------------------------------------------------------------------
+(movies_boot1_mean <- boot1 %>% summarize(mean = mean(rating)))
+
+## ------------------------------------------------------------------------
+do(10) * 
+  (resample(movies_sample) %>% 
+     summarize(mean = mean(rating)))
+
+## ---- include=FALSE------------------------------------------------------
+if(!file.exists("rds/trials.rds")){
+  trials <- do(5000) * summarize(resample(movies_sample), mean = mean(rating))
+  saveRDS(object = trials, "rds/trials.rds")
+} else {
+  trials <- readRDS("rds/trials.rds")
+}
+
+## ---- eval=FALSE---------------------------------------------------------
+## trials <- do(5000) * summarize(resample(movies_sample), mean = mean(rating))
+
+## ---- fig.cap="Bootstrapped means histogram"-----------------------------
+ggplot(data = trials, mapping = aes(x = mean)) +
+  geom_histogram(bins = 30, color = "white")
+
+## ------------------------------------------------------------------------
+(ciq_mean_rating <- confint(trials, level = 0.95, method = "quantile"))
+
+## ------------------------------------------------------------------------
+movies %>% summarize(mean_rating = mean(rating))
+
+## ----warning=FALSE, message=FALSE----------------------------------------
+(cise_mean_rating <- confint(trials, level = 0.95, method = "stderr"))
+
+## ----message=FALSE, warning=FALSE----------------------------------------
+(movies_trimmed <- movies %>% select(title, year, rating, Action, Romance))
+
+## ------------------------------------------------------------------------
+movies_trimmed <- movies_trimmed %>%
+  filter(!(Action == 1 & Romance == 1))
+
+## ------------------------------------------------------------------------
+movies_trimmed <- movies_trimmed %>%
+  mutate(genre = ifelse(Action == 1, "Action",
+                        ifelse(Romance == 1, "Romance",
+                               "Neither"))) %>%
+  filter(genre != "Neither") %>%
+  select(-Action, -Romance)
+
+## ------------------------------------------------------------------------
+set.seed(2017)
+movies_genre_sample <- movies_trimmed %>% 
+  group_by(genre) %>%
+  sample_n(34) %>% 
+  ungroup()
+
+## ------------------------------------------------------------------------
+mean_ratings <- movies_genre_sample %>% 
+  group_by(genre) %>%
+  summarize(mean = mean(rating))
+obs_diff <- diff(mean_ratings$mean)
+
+## ----message=FALSE, warning=FALSE----------------------------------------
+shuffled_ratings <- #movies_trimmed %>%
+  movies_genre_sample %>% 
+     mutate(genre = shuffle(genre)) %>% 
+     group_by(genre) %>%
+     summarize(mean = mean(rating))
+diff(shuffled_ratings$mean)
+
+## ----include=FALSE-------------------------------------------------------
+set.seed(2017)
+if(!file.exists("rds/many_shuffles.rds")){
+  many_shuffles <- do(5000) * 
+    (movies_genre_sample %>% 
+     mutate(genre = shuffle(genre)) %>% 
+      group_by(genre) %>%
+      summarize(mean = mean(rating))
+    )
+   saveRDS(object = many_shuffles, "rds/many_shuffles.rds")
+} else {
+   many_shuffles <- readRDS("rds/many_shuffles.rds")
+}
+
+## ----eval=FALSE----------------------------------------------------------
+## set.seed(2017)
+## many_shuffles <- do(5000) *
+##   (movies_genre_sample %>%
+##      mutate(genre = shuffle(genre)) %>%
+##      group_by(genre) %>%
+##      summarize(mean = mean(rating))
+##    )
+
+## ------------------------------------------------------------------------
+rand_distn <- many_shuffles %>%
+  group_by(.index) %>%
+  summarize(diffmean = diff(mean))
+head(rand_distn, 10)
+
+## ----fig.cap="Simulated shuffled sample means histogram"-----------------
+ggplot(data = rand_distn, mapping = aes(x = diffmean)) +
+  geom_histogram(color = "white", bins = 20)
+
+## ------------------------------------------------------------------------
+(std_err <- rand_distn %>% summarize(se = sd(diffmean)))
+
+## ------------------------------------------------------------------------
+(lower <- obs_diff - (2 * std_err))
+(upper <- obs_diff + (2 * std_err))
+
+## ------------------------------------------------------------------------
+df1 <- data_frame(samp1 = rexp(50))
+df2 <- data_frame(samp2 = rnorm(100))
+df3 <- data_frame(samp3 = rbeta(20, 5, 5))
+
