e4101c8cd980d28df8d5caf13c7e44d80ffdf8fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-26 15:24:54 +0000

    Deploy moderndive/moderndive_book to github.com/moderndive/moderndive_book.git:gh-pages

--- scripts/03-visualization.R
+++ scripts/03-visualization.R
@@ -73,8 +73,14 @@ ggplot(data = all_alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) +
   geom_point(alpha = 0.2)
 
 ## ----jitter, fig.cap="Jittered delay scatterplot"------------------------
-ggplot(all_alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+ggplot(data = all_alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
   geom_jitter(width = 30, height = 30)
+
+## ---- eval = FALSE-------------------------------------------------------
+## ggplot(data = all_alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) +
+##   geom_jitter(width = 30, height = 30)
+## ggplot(all_alaska_flights, aes(x = dep_delay, y = arr_delay)) +
+##   geom_jitter(width = 30, height = 30)
 
 ## **Learning Check Solutions**
 
