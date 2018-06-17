e00815fb8b0d864b68aac2a326d5843ff7d4fbfe
ismayc <chester.ismay@gmail.com>
2018-06-16 18:09:45 -0700

    Update bookdown and add infer images

--- docs/scripts/09-confidence-intervals.R
+++ docs/scripts/09-confidence-intervals.R
@@ -2,6 +2,8 @@
 library(dplyr)
 library(ggplot2)
 library(moderndive)
+# remotes::install_github("andrewpbray/infer", ref = "p_value")
+library(infer)
 # For loading CSV files:
 library(readr)
 
@@ -9,293 +11,125 @@ library(readr)
 # Packages needed internally, but not in text.
 library(knitr)
 
-## ---- message=FALSE, warning=FALSE---------------------------------------
-library(dplyr)
-library(ggplot2)
-library(infer)
-
-# Clean data
-mtcars <- mtcars %>%
-  as_tibble() %>% 
-  mutate(am = factor(am))
-
-# Simulate sampling distribution of two-sample difference in means:
-sampling_distribution <- mtcars %>%
-  specify(mpg ~ am) %>%
-  generate(reps = 1000, type = "bootstrap") %>%
-  calculate(stat = "diff in means", order = c("1", "0")) 
-
-# Compute 95% confidence interval:
-conf_int <- sampling_distribution %>% 
-  pull(stat) %>% 
-  quantile(probs = c(0.025, 0.975))
-
-# Visualize:
-plot <- sampling_distribution %>% 
-  visualize()
-plot +
-  geom_vline(xintercept = conf_int, col = "red", size = 1)
-
-## ---- eval=FALSE, message=FALSE, warning=FALSE---------------------------
-## library(readr)
-## tactile_prop_red <- read_csv("https://rudeboybert.github.io/STAT135/static/sampling_red_balls.csv")
-
-## ---- eval=FALSE, message=FALSE, warning=FALSE---------------------------
-## conf_ints <- tactile_prop_red %>%
-##   rename(p_hat = prop_red) %>%
-##   mutate(
-##     n = 50,
-##     SE = sqrt(p_hat*(1-p_hat)/n),
-##     MoE = 1.96 * SE,
-##     conf_low = p_hat - MoE,
-##     conf_high = p_hat + MoE
-##   )
-## conf_ints
-
-## ---- echo=FALSE, message=FALSE, warning=FALSE---------------------------
-conf_ints <- read_csv("https://rudeboybert.github.io/STAT135/static/sampling_red_balls.csv") %>% 
-  rename(p_hat = prop_red) %>% 
-  select(-replicate) %>% 
-  mutate(
-    n = 50, 
-    SE = sqrt(p_hat*(1-p_hat)/n),
-    MoE = 1.96*SE,
-    conf_low = p_hat - MoE,
-    conf_high = p_hat + MoE,
-    y = 1:n()
-  )
-conf_ints %>% 
-  select(-y) %>% 
-  kable(
-    digits = 3,
-    caption = "33 confidence intervals based on 33 tactile samples of size n=50", 
-    booktabs = TRUE
-  )
-
-## ----tactile-conf-int, echo=FALSE, message=FALSE, warning=FALSE, fig.cap= "33 confidence intervals based on 33 tactile samples of size n=50", fig.height=6----
-groups <- conf_ints$group
-conf_ints %>% 
-    mutate(
-      p = 900/2400,
-      captured = conf_low <= p & p <= conf_high
-    ) %>% 
-    ggplot() +
-    geom_point(aes(x=p_hat, y=y, col=captured)) +
-    geom_vline(xintercept=900/2400, col="red") +
-  geom_segment(aes(y=y, yend=y, x=conf_low, xend=conf_high, col=captured)) +
-  scale_y_continuous(breaks=1:33, labels=groups) +
-  labs(
-    x = expression("Proportion red"),
-    y = "",
-    title=expression(paste("95% confidence intervals for ", p, sep=""))
-  ) +
-  scale_color_manual(values=c("blue", "orange")) 
+## ----include=FALSE-------------------------------------------------------
+set.seed(2018)
+pennies_sample <- pennies %>% sample_n(40)
 
 ## ------------------------------------------------------------------------
-# First: Take 100 virtual samples of n=50 balls
-virtual_samples <- bowl %>% 
-  rep_sample_n(size = 50, reps = 100)
-
-# Second: For each virtual sample compute the proportion red
-virtual_prop_red <- virtual_samples %>% 
-  group_by(replicate) %>% 
-  summarize(red = sum(color == "red")) %>% 
-  mutate(prop_red = red / 50)
-
-# Third: Compute the 95% confidence interval as above
-virtual_prop_red <- virtual_prop_red %>% 
-  rename(p_hat = prop_red) %>% 
-  mutate(
-    n = 50,
-    SE = sqrt(p_hat*(1-p_hat)/n),
-    MoE = 1.96 * SE,
-    conf_low = p_hat - MoE,
-    conf_high = p_hat + MoE
-  )
-
-## ----virtual-conf-int, echo=FALSE, message=FALSE, warning=FALSE, fig.height=6, fig.cap="100 confidence intervals based on 100 virtual samples of size n=50"----
-set.seed(79)
-
-virtual_samples <- bowl %>% 
-  rep_sample_n(size = 50, reps = 100)
-
-# Second: For each virtual sample compute the proportion red
-virtual_prop_red <- virtual_samples %>% 
-  group_by(replicate) %>% 
-  summarize(red = sum(color == "red")) %>% 
-  mutate(prop_red = red / 50)
-
-# Third: Compute the 95% confidence interval as above
-virtual_prop_red <- virtual_prop_red %>% 
-  rename(p_hat = prop_red) %>% 
-  mutate(
-    n = 50,
-    SE = sqrt(p_hat*(1-p_hat)/n),
-    MoE = 1.96 * SE,
-    conf_low = p_hat - MoE,
-    conf_high = p_hat + MoE
-  ) %>% 
-  mutate(
-    y = 1:n(),
-    p = 900/2400,
-    captured = conf_low <= p & p <= conf_high
-  )
-
-ggplot(virtual_prop_red) +
-  geom_point(aes(x=p_hat, y=y, col=captured)) +
-  geom_segment(aes(y=y, yend=y, x=conf_low, xend=conf_high, col=captured)) +
-  labs(
-    x = expression("Proportion red"),
-    y = "Replicate ID",
-    title=expression(paste("95% confidence intervals for ", p, sep=""))
-  ) +
-  scale_color_manual(values=c("blue", "orange")) + 
-  geom_vline(xintercept=900/2400, col="red") 
-
-## ----message=FALSE, warning=FALSE----------------------------------------
-library(dplyr)
-library(ggplot2)
-library(infer)
-library(mosaic)
-library(knitr)
-library(ggplot2movies)
-
-## ----message=FALSE, warning=FALSE, echo=FALSE----------------------------
-# Packages needed internally, but not in text.
-
-## ----fig.cap="Population ratings histogram"------------------------------
-movies %>% ggplot(aes(x = rating)) +
-  geom_histogram(color = "white", bins = 20)
-
-## **_Learning check_**
+pennies_sample
 
 ## ------------------------------------------------------------------------
-set.seed(2017)
-movies_sample <- movies %>% 
-  sample_n(50)
-
-## ----fig.cap="Sample ratings histogram"----------------------------------
-ggplot(data = movies_sample, aes(x = rating)) +
-  geom_histogram(color = "white", bins = 20)
+ggplot(pennies_sample, aes(x = age_in_2011)) +
+  geom_histogram(bins = 10, color = "white")
 
 ## ------------------------------------------------------------------------
-(movies_sample_mean <- movies_sample %>% 
-   summarize(mean = mean(rating)))
-
-## ------------------------------------------------------------------------
-boot1 <- resample(movies_sample) %>%
-  arrange(orig.id)
-
-## ------------------------------------------------------------------------
-(movies_boot1_mean <- boot1 %>% summarize(mean = mean(rating)))
-
-## ------------------------------------------------------------------------
-do(10) * 
-  (resample(movies_sample) %>% 
-     summarize(mean = mean(rating)))
-
-## ---- include=FALSE------------------------------------------------------
-if(!file.exists("rds/trials.rds")){
-  trials <- do(5000) * summarize(resample(movies_sample), mean = mean(rating))
-  saveRDS(object = trials, "rds/trials.rds")
-} else {
-  trials <- readRDS("rds/trials.rds")
-}
-
-## ---- eval=FALSE---------------------------------------------------------
-## trials <- do(5000) * summarize(resample(movies_sample), mean = mean(rating))
-
-## ---- fig.cap="Bootstrapped means histogram"-----------------------------
-ggplot(data = trials, mapping = aes(x = mean)) +
-  geom_histogram(bins = 30, color = "white")
-
-## ------------------------------------------------------------------------
-(ciq_mean_rating <- confint(trials, level = 0.95, method = "quantile"))
-
-## ------------------------------------------------------------------------
-movies %>% summarize(mean_rating = mean(rating))
-
-## ----warning=FALSE, message=FALSE----------------------------------------
-(cise_mean_rating <- confint(trials, level = 0.95, method = "stderr"))
-
-## ----message=FALSE, warning=FALSE----------------------------------------
-(movies_trimmed <- movies %>% select(title, year, rating, Action, Romance))
-
-## ------------------------------------------------------------------------
-movies_trimmed <- movies_trimmed %>%
-  filter(!(Action == 1 & Romance == 1))
-
-## ------------------------------------------------------------------------
-movies_trimmed <- movies_trimmed %>%
-  mutate(genre = ifelse(Action == 1, "Action",
-                        ifelse(Romance == 1, "Romance",
-                               "Neither"))) %>%
-  filter(genre != "Neither") %>%
-  select(-Action, -Romance)
-
-## ------------------------------------------------------------------------
-set.seed(2017)
-movies_genre_sample <- movies_trimmed %>% 
-  group_by(genre) %>%
-  sample_n(34) %>% 
-  ungroup()
-
-## ------------------------------------------------------------------------
-mean_ratings <- movies_genre_sample %>% 
-  group_by(genre) %>%
-  summarize(mean = mean(rating))
-obs_diff <- diff(mean_ratings$mean)
-
-## ----message=FALSE, warning=FALSE----------------------------------------
-shuffled_ratings <- #movies_trimmed %>%
-  movies_genre_sample %>% 
-     mutate(genre = shuffle(genre)) %>% 
-     group_by(genre) %>%
-     summarize(mean = mean(rating))
-diff(shuffled_ratings$mean)
+x_bar <- pennies_sample %>% 
+  summarize(stat = mean(age_in_2011))
+x_bar
 
 ## ----include=FALSE-------------------------------------------------------
-set.seed(2017)
-if(!file.exists("rds/many_shuffles.rds")){
-  many_shuffles <- do(5000) * 
-    (movies_genre_sample %>% 
-     mutate(genre = shuffle(genre)) %>% 
-      group_by(genre) %>%
-      summarize(mean = mean(rating))
-    )
-   saveRDS(object = many_shuffles, "rds/many_shuffles.rds")
-} else {
-   many_shuffles <- readRDS("rds/many_shuffles.rds")
-}
-
-## ----eval=FALSE----------------------------------------------------------
-## set.seed(2017)
-## many_shuffles <- do(5000) *
-##   (movies_genre_sample %>%
-##      mutate(genre = shuffle(genre)) %>%
-##      group_by(genre) %>%
-##      summarize(mean = mean(rating))
-##    )
+set.seed(201)
 
 ## ------------------------------------------------------------------------
-rand_distn <- many_shuffles %>%
-  group_by(.index) %>%
-  summarize(diffmean = diff(mean))
-head(rand_distn, 10)
-
-## ----fig.cap="Simulated shuffled sample means histogram"-----------------
-ggplot(data = rand_distn, mapping = aes(x = diffmean)) +
-  geom_histogram(color = "white", bins = 20)
+bootstrap_sample1 <- pennies_sample %>% 
+  rep_sample_n(size = 40, replace = TRUE, reps = 1)
+bootstrap_sample1
 
 ## ------------------------------------------------------------------------
-(std_err <- rand_distn %>% summarize(se = sd(diffmean)))
+ggplot(bootstrap_sample1, aes(x = age_in_2011)) +
+  geom_histogram(bins = 10, color = "white")
 
 ## ------------------------------------------------------------------------
-(lower <- obs_diff - (2 * std_err))
-(upper <- obs_diff + (2 * std_err))
+bootstrap_sample1 %>% 
+  summarize(stat = mean(age_in_2011))
 
 ## ------------------------------------------------------------------------
-df1 <- data_frame(samp1 = rexp(50))
-df2 <- data_frame(samp2 = rnorm(100))
-df3 <- data_frame(samp3 = rbeta(20, 5, 5))
+six_bootstrap_samples <- pennies_sample %>% 
+  rep_sample_n(size = 40, replace = TRUE, reps = 6)
+
+## ------------------------------------------------------------------------
+ggplot(six_bootstrap_samples, aes(x = age_in_2011)) +
+  geom_histogram(bins = 10, color = "white") +
+  facet_wrap(~ replicate)
+
+## ------------------------------------------------------------------------
+six_bootstrap_samples %>% 
+  group_by(replicate) %>% 
+  summarize(stat = mean(age_in_2011))
+
+## ----fig.align='center', echo=FALSE--------------------------------------
+knitr::include_graphics("images/flowcharts/infer/specify.png")
+
+## ------------------------------------------------------------------------
+pennies_sample %>% 
+  specify(response = age_in_2011)
+
+## ------------------------------------------------------------------------
+pennies_sample %>% 
+  specify(formula = age_in_2011 ~ NULL)
+
+## ----fig.align='center', echo=FALSE--------------------------------------
+knitr::include_graphics("images/flowcharts/infer/generate.png")
+
+## ------------------------------------------------------------------------
+thousand_bootstrap_samples <- pennies_sample %>% 
+  specify(response = age_in_2011) %>% 
+  generate(reps = 1000)
+
+## ------------------------------------------------------------------------
+thousand_bootstrap_samples %>% count(replicate)
+
+## ----fig.align='center', echo=FALSE--------------------------------------
+knitr::include_graphics("images/flowcharts/infer/calculate.png")
+
+## ------------------------------------------------------------------------
+bootstrap_distribution <- pennies_sample %>% 
+  specify(response = age_in_2011) %>% 
+  generate(reps = 1000) %>% 
+  calculate(stat = "mean")
+bootstrap_distribution
+
+## ------------------------------------------------------------------------
+pennies_sample %>% 
+  summarize(stat = mean(age_in_2011))
+
+## ------------------------------------------------------------------------
+pennies_sample %>% 
+  specify(response = age_in_2011) %>% 
+  calculate(stat = "mean")
+
+## ----fig.align='center', echo=FALSE--------------------------------------
+knitr::include_graphics("images/flowcharts/infer/visualize.png")
+
+## ------------------------------------------------------------------------
+bootstrap_distribution %>% visualize()
+
+## ------------------------------------------------------------------------
+bootstrap_distribution %>% visualize(obs_stat = x_bar)
+
+## ------------------------------------------------------------------------
+bootstrap_distribution %>% 
+  summarize(mean_of_means = mean(stat))
+
+## ------------------------------------------------------------------------
+bootstrap_distribution %>% 
+  get_ci(level = 0.95, type = "percentile")
+
+## ------------------------------------------------------------------------
+percentile_ci <- bootstrap_distribution %>% 
+  get_ci()
+percentile_ci
+
+## ------------------------------------------------------------------------
+bootstrap_distribution %>% 
+  visualize(endpoints = percentile_ci, direction = "between")
+
+## ------------------------------------------------------------------------
+standard_error_ci <- bootstrap_distribution %>% 
+  get_ci(type = "se", point_estimate = x_bar)
+standard_error_ci
+
+## ------------------------------------------------------------------------
+bootstrap_distribution %>% 
+  visualize(endpoints = standard_error_ci, direction = "between")
 
