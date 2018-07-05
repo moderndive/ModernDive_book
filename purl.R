8dfc16f43c9ffe3c59c419e860f05a72f75f6a03
ismayc <chester.ismay@gmail.com>
2018-07-05 12:37:15 -0700

    Convert to seq_along

--- purl.R
+++ purl.R
@@ -7,7 +7,7 @@ chapter_titles <- c("getting-started", "visualization", "tidy", "wrangling",
                     "inference-for-regression", "thinking-with-data")
 chapter_numbers <- stringr::str_pad(2:(length(chapter_titles) + 1), 2,
                                     "left", pad = "0")
-for(i in 1:length(chapter_numbers)){
+for(i in seq_along(length(chapter_numbers))){
   Rmd_file <- stringr::str_c(chapter_numbers[i], "-",
                              chapter_titles[i], ".Rmd")
   R_file <- stringr::str_c("docs/scripts/", chapter_numbers[i],
