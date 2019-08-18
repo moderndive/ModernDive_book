d5313a51b9648d53cadc811d0d486a09ae063530
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-18 22:21:46 +0000

    Deploy moderndive/moderndive_book to github.com/moderndive/moderndive_book.git:gh-pages

--- images/ss/README.md
+++ images/ss/README.md
@@ -1,1 +1,4 @@
-A catalog of all shutterstock images used can be found on [Google Sheets](https://docs.google.com/spreadsheets/d/1gmapY82wFKvCuY9tSXi4UQeoEfswbhIR-3e4RGUuMKg/edit#gid=1673427429).
+A catalog of all shutterstock images used can be found on [Google Sheets](https://docs.google.com/spreadsheets/d/1gmapY82wFKvCuY9tSXi4UQeoEfswbhIR-3e4RGUuMKg/edit#gid=1673427429). Note that
+
+* `wrapper_function.png` is an edited version of `560016454.png`
+* `head.png` is a composite of `207112555.png` and `1317323615.png`
---
+++ scripts/01-getting-started.R
@@ -0,0 +1,77 @@
+## ----message=FALSE, warning=FALSE, echo=FALSE----------------------------
+# Packages needed internally, but not in text.
+library(scales)
+
+
+
+
+
+
+
+
+
+
+
+
+## **_Learning check_**
+
+
+## NA
+
+## ---- eval=FALSE---------------------------------------------------------
+## library(ggplot2)
+
+
+## **_Learning check_**
+
+
+## NA
+
+## ----message=FALSE-------------------------------------------------------
+library(nycflights13)
+library(dplyr)
+library(knitr)
+
+
+## ----load_flights--------------------------------------------------------
+flights
+
+
+## **_Learning check_**
+
+
+## NA
+
+## ------------------------------------------------------------------------
+glimpse(flights)
+
+
+## **_Learning check_**
+
+
+## NA
+
+## ----eval=FALSE----------------------------------------------------------
+## airlines
+## kable(airlines)
+
+
+## ----eval=FALSE----------------------------------------------------------
+## airlines$name
+
+
+## ------------------------------------------------------------------------
+glimpse(airports)
+
+
+## **_Learning check_**
+
+
+
+
+## ----eval=FALSE----------------------------------------------------------
+## ?flights
+
+
+## **_Learning check_**
+
